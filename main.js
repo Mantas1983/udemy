5ca9febf41b034dcbe49fa5c8d3b541a65b843f8
Mantas1983 <linkevicius83@gmail.com>
2022-02-22 20:31:05 +0000

    pirmas praktinis darbas

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-console.log("hello world");
-let unserName = 'petras';
-console.log(unserName);
+const numberOfFilms = +prompt('kiek filmu jus praziurejote', '');
+const personalMovieDB = {
+  count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
+};
+const a = prompt('koks jusu paskutinis matytas filmas', '');
+      b = prompt('keliais balais vertinate ji?', ''),
+      c = prompt('koks jusu paskutinis matytas filmas', ''),
+      d = prompt('keliais balais vertinate ji?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
+console.log(personalMovieDB);
